2a5f88da594ba397caf668fa49e9c852c1a7e0a9
ImGhostCode <imghostcode@gmail.com>
2023-01-03 11:05:19 -0500

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -13,6 +14,16 @@ app.use("/api/contact", contactRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+
+app.use((req, res, next) => {
+  next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
